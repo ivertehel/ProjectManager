88de1a71c4ff3499fab66f70a95e70f7949f7de5
Ivan <sirius9764@gmail.com>
2015-12-12 00:40:49 +0200

    Added SkillVM

---
+++ ProjectManager/ViewModel/Data/SkillVM.cs
@@ -0,0 +1,48 @@
+﻿using PMDataLayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMView.View.WrapperVM
+{
+    public class SkillVM : BaseVM
+    {
+        private Skill _skill;
+
+        public SkillVM(Skill skill)
+        {
+            _skill = skill;
+        }
+
+        public Skill Skill
+        {
+            get { return _skill; }
+            set { _skill = value; }
+        }
+
+        public string Name
+        {
+            get { return _skill.Name; }
+            set { _skill.Name = value; }
+        }
+
+        public IEnumerable<User> Users
+        {
+            get { return _skill.Users; }
+        }
+
+        public IEnumerable<Project> Projects
+        {
+            get { return _skill.Projects; }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
