c027ebe335830a13e91d35426e71ecaffdb7f2ec
Ivan <sirius9764@gmail.com>
2016-02-11 15:00:33 +0200

    Auth was added

--- ProjectManagerSite/ProjectManagerSite/ProjectManagerSite/Controllers/HomeController.cs
+++ ProjectManagerSite/ProjectManagerSite/ProjectManagerSite/Controllers/HomeController.cs
@@ -1,46 +1,91 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PMDataLayer;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using UserStore.BLL.Interfaces;
 
-namespace ProjectManagerSite.Controllers
+namespace UserStore.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
+        private IUserService UserService
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+            }
+        }
+
         public ActionResult Index()
         {
-            return View("MyProfile");
+            return View();
         }
-
-        public ActionResult MyProfileEdit()
+        [Authorize(Roles = "admin")]
+        public ActionResult About()
         {
-            return PartialView("MyProfileEdit", PMDataLayer.User.Items[0]);
-        }
+            ViewBag.Message = "Your application description page.";
 
-        public ActionResult MyProfileSave()
-        {
-            return View("MyProfile");
-        }
-
-        public ActionResult MyTeams()
-        {
-            ViewBag.TeamsPage = "active";
-            return View("MyTeams");
-        }
-
-        public ActionResult MyProjects()
-        {
-            ViewBag.ProjectsPage = "active";
-            return View("MyProjects");
-        }
-
-        public ActionResult MyTasks()
-        {
-            ViewBag.TasksPage = "active";
-            return View("MyTasks");
+            return View();
         }
     }
 }
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using PMDataLayer;
+//using Microsoft.AspNet.Identity.Owin;
+//using UserStore.BLL.Interfaces;
+
+//namespace ProjectManagerSite.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        private IUserService UserService
+//        {
+//            get
+//            {
+//                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+//            }
+//        }
+
+//        [Authorize]
+//        public ActionResult Index()
+//        {
+//            return View("MyProfile");
+//        }
+
+//        public ActionResult MyProfileEdit()
+//        {
+//            return PartialView("MyProfileEdit", PMDataLayer.User.Items[0]);
+//        }
+
+//        public ActionResult MyProfileSave()
+//        {
+//            return View("MyProfile");
+//        }
+
+//        public ActionResult MyTeams()
+//        {
+//            ViewBag.TeamsPage = "active";
+//            return View("MyTeams");
+//        }
+
+//        public ActionResult MyProjects()
+//        {
+//            ViewBag.ProjectsPage = "active";
+//            return View("MyProjects");
+//        }
+
+//        public ActionResult MyTasks()
+//        {
+//            ViewBag.TasksPage = "active";
+//            return View("MyTasks");
+//        }
+//    }
+//}
