df1a656241af249a8f4fe03ef13dac5b5d031b81
Ivan <sirius9764@gmail.com>
2016-02-23 12:23:02 +0200

    View and project controller have been added

---
+++ ProjectManagerSite/ProjectManagerSite/ProjectManagerSite/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using ProjectManagerSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagerSite.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadOrder(string OrderId)
+        {
+            var model = new OrderVM(User, OrderId);
+            return View("Order", model);
+        }
+    }
+}
---
+++ ProjectManagerSite/ProjectManagerSite/ProjectManagerSite/Models/OrderVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagerSite.Models
+{
+    public class OrderVM : BaseVM
+    {
+        public Guid OrderId { get; set; }
+
+        public OrderVM(IPrincipal user, string orderId) : base(user)
+        {
+            OrderId = new Guid(orderId);
+        }
+
+        public OrderVM() : base(null)
+        {
+
+        }
+
+    }
+}
